79db2075519b5051494013fb975310021d6ac539
Abbas Alsalman <alsaedy54321@gmail.com>
2022-11-08 21:08:15 -0500

    weatherino

--- app/src/main/java/com/csi4999/visionaryalarmclock/fragment/WeatherFragment.kt
+++ app/src/main/java/com/csi4999/visionaryalarmclock/fragment/WeatherFragment.kt
@@ -12,12 +12,13 @@ class WeatherFragment : Fragment() {
     lateinit var fragmentMapBinding: FragmentMapsBinding
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        fragmentMapBinding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_maps, container, false)
+       // fragmentMapBinding =
+          //  DataBindingUtil.inflate(inflater, R.layout.fragment_maps, container, false)
 
         return fragmentMapBinding.root
     }
