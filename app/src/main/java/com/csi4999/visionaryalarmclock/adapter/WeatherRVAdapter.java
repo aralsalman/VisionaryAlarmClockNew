792dbda14f09680f89c48f8ce9d3cfa16aef7c12
LenzA99 <71934563+LenzA99@users.noreply.github.com>
2022-11-08 20:16:09 -0500

    weather shit

---
+++ app/src/main/java/com/csi4999/visionaryalarmclock/adapter/WeatherRVAdapter.java
@@ -0,0 +1,76 @@
+package com.csi4999.visionaryalarmclock.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.csi4999.visionaryalarmclock.R;
+import com.csi4999.visionaryalarmclock.activity.WeatherActivity;
+import com.csi4999.visionaryalarmclock.model.WeatherRVModal;
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class WeatherRVAdapter extends RecyclerView.Adapter<WeatherRVAdapter.ViewHolder>{
+    private Context context;
+    private ArrayList<WeatherRVModal> weatherRVModalArrayList;
+
+    public WeatherRVAdapter(Context context, ArrayList<WeatherRVModal> weatherRVModalArrayList) {
+        this.context = context;
+        this.weatherRVModalArrayList = weatherRVModalArrayList;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.weather_rv_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        WeatherRVModal modal = weatherRVModalArrayList.get(position);
+        holder.temperatureTV.setText(modal.getTemperature()+"°F");
+        Picasso.get().load(modal.getIcon()).into(holder.conditionIV);
+        holder.windTV.setText(modal.getWindSpeed()+"mph");
+        SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat output1 = new SimpleDateFormat("MM-dd-yyyy");
+        SimpleDateFormat output2 = new SimpleDateFormat("hh:mm aa");
+        try{
+            Date t = input.parse(modal.getTime());
+            holder.timeTV.setText(output2.format(t));
+            //holder.dateTV.setDate(output1.format(t));
+        } catch (ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return weatherRVModalArrayList.size();
+    }
+
+    protected class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView windTV, temperatureTV, timeTV;
+        private ImageView conditionIV;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            windTV=itemView.findViewById(R.id.idTVWindSpeed);
+            temperatureTV=itemView.findViewById(R.id.idTVTemperature);
+            timeTV=itemView.findViewById(R.id.idTVTime);
+            conditionIV=itemView.findViewById(R.id.idIVCondition);
+        }
+    }
+}
